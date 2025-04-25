19a526ffc54dd6115c6b0b510a7542ac21d50e7e
Wazingwa Mugala <wazingwamugala90@gmail.com>
2025-04-25 11:36:31 +0200

    added the datapickle fie to the gitignore file to prevent from being ushed to git

--- create_dataset.py
+++ create_dataset.py
@@ -1,65 +1,50 @@
+import os
+import pickle
+
 import mediapipe as mp
 import cv2
-import os
 import matplotlib.pyplot as plt
 
-# Initialize MediaPipe Hands
+
 mp_hands = mp.solutions.hands
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 
-hands = mp_hands.Hands(
-    static_image_mode=True,
-    max_num_hands=2,
-    min_detection_confidence=0.7  # Increased for better accuracy
-)
+hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.3)
 
 DATA_DIR = './data'
 
-# Loop through each folder in ./data
-for dir_name in os.listdir(DATA_DIR):
-    dir_path = os.path.join(DATA_DIR, dir_name)
-    if not os.path.isdir(dir_path):
-        continue
+data = []
+labels = []
+for dir_ in os.listdir(DATA_DIR):
+    for img_path in os.listdir(os.path.join(DATA_DIR, dir_)):
+        data_aux = []
 
-    image_files = os.listdir(dir_path)
-    if not image_files:
-        print(f"No images found in {dir_path}")
-        continue
+        x_ = []
+        y_ = []
 
-    img_path = os.path.join(dir_path, image_files[1])
-    print(f"Processing image: {img_path}")
+        img = cv2.imread(os.path.join(DATA_DIR, dir_, img_path))
+        img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-    # Read the image
-    img = cv2.imread(img_path)
-    if img is None:
-        print(f"Failed to read image: {img_path}")
-        continue
+        results = hands.process(img_rgb)
+        if results.multi_hand_landmarks:
+            for hand_landmarks in results.multi_hand_landmarks:
+                for i in range(len(hand_landmarks.landmark)):
+                    x = hand_landmarks.landmark[i].x
+                    y = hand_landmarks.landmark[i].y
 
-    # Convert to RGB for MediaPipe
-    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    results = hands.process(img_rgb)
+                    x_.append(x)
+                    y_.append(y)
 
-    # Draw hand landmarks if found
-    if results.multi_hand_landmarks:
-        for hand_landmarks in results.multi_hand_landmarks:
-            mp_drawing.draw_landmarks(
-                img_rgb,
-                hand_landmarks,
-                mp_hands.HAND_CONNECTIONS,
-                mp_drawing_styles.get_default_hand_landmarks_style(),
-                mp_drawing_styles.get_default_hand_connections_style()
-            )
-        print("Hand landmarks detected.")
-    else:
-        print("No hand landmarks detected.")
+                for i in range(len(hand_landmarks.landmark)):
+                    x = hand_landmarks.landmark[i].x
+                    y = hand_landmarks.landmark[i].y
+                    data_aux.append(x - min(x_))
+                    data_aux.append(y - min(y_))
 
-    # Display the image using matplotlib
-    plt.figure(figsize=(8, 6))
-    plt.imshow(img_rgb)
-    plt.axis('off')  # Hide axes
-    plt.title(f'Hand Landmarks - {image_files[0]}')
+            data.append(data_aux)
+            labels.append(dir_)
 
-# Clean up
-hands.close()
-plt.show()
+f = open('data.pickle', 'wb')
+pickle.dump({'data': data, 'labels': labels}, f)
+f.close()
